199cef16a7dca68937a8154ccfff77d5aec90844
Marisol Antunez <mariantunez@users.noreply.github.com>
2022-02-23 17:36:10 -0800

    update file merge conflict

--- sql_files/select_tables.sql
+++ sql_files/select_tables.sql
@@ -5,10 +5,12 @@ SELECT * FROM User;
 SELECT * FROM Host;
 SELECT * FROM Guest;
 SELECT * FROM Review;
+SELECT * FROM Neighorhood;
 SELECT * FROM ListingRating;
 
 SELECT COUNT(*) FROM User;
 SELECT COUNT(*) FROM Host;
 SELECT COUNT(*) FROM Guest;
 SELECT COUNT(*) FROM Review;
+SELECT COUNT(*) FROM Neighborhood;
 SELECT COUNT(*) FROM ListingRating;
